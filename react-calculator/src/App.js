6d544d31b9788e1e877b1df074f0d1ff202baee7
Nhlalala <nhlalala.mudanisi@dadevs.co.za>
2023-03-09 12:48:25 +0200

    added

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -1,6 +1,19 @@
 import { useReducer } from "react";
+import DigitButton from "./DigitButton"
+import OperationButton from "./OperationButton"
 import "./styles.css"
 
+export const ACTIONS ={
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+}
+
+function reducer(state, {type,payload}){
+  
+}
 
 function App() {
   
